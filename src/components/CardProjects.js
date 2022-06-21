b27d072571f38d183d7069b90078d3c592eb2f63
vinidipaula <vinidipaula92@gmail.com>
2022-06-21 19:13:16 -0300

    :lipstick: feat: Padronização de fonte family

--- src/components/CardProjects.js
+++ src/components/CardProjects.js
@@ -6,37 +6,44 @@ import { FiExternalLink } from 'react-icons/fi';
 export default function CardProjects() {
   return (
     <div className="container">
-      {
-        project.map((project, index) => (
-          <div className="row card-projetos">
-            <div className="col-md-4">
-              <img src={project.image} alt={project.title} key={index} />
-            </div>
-            <div className="col-md-8">
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <ul>
-                {
-                  project.technologies.map((tech, index) => (
-                  <li key={index}>{tech}</li>
-                ))}
-              </ul>
-              <div className="row text-center">
-                <div className="col-3">
-                  <a href={project.github} target="_blank" rel="noopener noreferrer">
-                    <BsGithub size={30} color="black"/>
-                  </a>
-                </div>
-                <div className="col-3">
-                  <a href={project.link} target="_blank" rel="noopener noreferrer">
-                    <FiExternalLink size={30} color="black"/>
-                  </a>
-                </div>
+      {project.map((project, index) => (
+        <div className="row card-projetos">
+          <div className="col-md-4">
+            <img src={project.image} alt={project.title} key={index} />
+          </div>
+          <div className="col-md-8">
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <ul>
+              {project.technologies.map((tech, index) => (
+                <li key={index} className="my-techs">
+                  {tech}
+                </li>
+              ))}
+            </ul>
+            <div className="row text-center">
+              <div className="col-3">
+                <a
+                  href={project.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <BsGithub size={30} color="black" />
+                </a>
+              </div>
+              <div className="col-3">
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FiExternalLink size={30} color="black" />
+                </a>
               </div>
             </div>
           </div>
-        ))
-      }
+        </div>
+      ))}
     </div>
   );
 }
